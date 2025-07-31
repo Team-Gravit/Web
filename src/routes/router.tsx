8d534bf8fe7612606a321d3e2a030717fcabd0b7
hyewon <helena030828@naver.com>
2025-07-31 20:52:22 +0900

    로그인페이지 퍼블리싱 완료

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Root from './Root';
 import MainPage from '../pages/MainPage';
+import LoginPage from '../pages/LoginPage';
+import SetInfoPage from '../pages/SetInfoPage';
+import SuccessPage from '../pages/SuccessPage';
 import UserPage from '../pages/UserPage';
 import StudyPage from '../pages/StudyPage';
 import LeaguePage from '../pages/LeaguePage';
@@ -13,6 +16,9 @@ const router = createBrowserRouter([
         element: <Root />,
         children: [
             { index: true, element: <MainPage /> },
+            { path: 'login', element: <LoginPage /> },
+            { path: 'set-info', element: <SetInfoPage /> },
+            { path: 'success', element: <SuccessPage /> },
             { path: 'user', element: <UserPage /> },
             { path: 'study', element: <StudyPage /> },
             { path: 'league', element: <LeaguePage /> },
