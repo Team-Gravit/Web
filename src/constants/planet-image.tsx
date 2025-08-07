d16eb4c89c844c112aba2c45dd973c7f36dc18fa
조하은 <jhe93@naver.com>
2025-08-07 20:47:18 +0900

    feat(#21): 이미지 매핑 시스템 구축

---
+++ src/constants/planet-image.tsx
@@ -0,0 +1,23 @@
+import MercuryImg from '@/assets/images/planets/Mercury.png';
+import VenusImg from '@/assets/images/planets/Venus.png';
+import EarthImg from '@/assets/images/planets/Earth.png';
+import MarsImg from '@/assets/images/planets/Mars.png';
+import JupiterImg from '@/assets/images/planets/Jupiter.png';
+import SaturnImg from '@/assets/images/planets/Saturn.png';
+import UranusImg from '@/assets/images/planets/Uranus.png';
+import NeptuneImg from '@/assets/images/planets/Neptune.png';
+import MoonImg from '@/assets/images/planets/Moon.png';
+
+export const PLANET_IMG_MAP = {
+    1: MercuryImg,
+    2: VenusImg,
+    3: EarthImg,
+    4: MarsImg,
+    5: JupiterImg,
+    6: SaturnImg,
+    7: UranusImg,
+    8: NeptuneImg,
+    9: MoonImg,
+} as const;
+
+export type PlanetId = keyof typeof PLANET_IMG_MAP;
