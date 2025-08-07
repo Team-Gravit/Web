1e3f2adfeda74dac3231c9bbc8a6fbbb056da1c0
조하은 <jhe93@naver.com>
2025-08-07 22:06:29 +0900

    design(#24): 배너 CSS 수정

--- src/components/@common/banner/Banner.tsx
+++ src/components/@common/banner/Banner.tsx
@@ -5,14 +5,16 @@ import Logo from '@/assets/logo/white-logo.svg?react';
 function Banner() {
     return (
         <section
-            className="w-full h-[300px] bg-cover bg-center flex flex-col justify-center"
+            className="w-full h-[370px] bg-cover bg-no-repeat bg-center flex flex-col justify-center overflow-hidden "
             style={{ backgroundImage: `url(${BannerImage})` }}
         >
-            {/* <img src={BannerImage} alt="그래빗 배너 이미지" className="w-full h-full object-cover" /> */}
-            <div className="flex flex-col items-start justify-center z-10 text-white pl-[147px] gap-4">
-                <Logo w={370} />
-                <p className="font-bold text-xl text-white">그래빗과 함께 CS 지식을 마스터해요!</p>
+            <div className="w-full h-full relative pt-[76px]">
+                <div className="absolute flex flex-col items-start justify-center z-10 text-white left-[147px] top-1/2 transform -translate-y-1/2 gap-4 pt-[76px]">
+                    <Logo width={370} />
+                    <p className="font-bold text-xl text-white">그래빗과 함께 CS 지식을 마스터해요!</p>
+                </div>
             </div>
+            {/* <img src={BannerImage} alt="그래빗 배너 이미지" className="w-full h-full object-cover" /> */}
         </section>
     );
 }
