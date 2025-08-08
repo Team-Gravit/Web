10f24460948f8fa6b3ee53a670efb0b7eefaad7e
조하은 <jhe93@naver.com>
2025-08-08 18:30:46 +0900

    fix: min-h 추가

--- src/components/@common/level-info/LevelProgressBar.tsx
+++ src/components/@common/level-info/LevelProgressBar.tsx
@@ -5,9 +5,9 @@ type LevelProgressBarProps = {
 
 const LevelProgressBar = ({ completed, level }: LevelProgressBarProps) => {
     return (
-        <div className="relative flex-grow h-full bg-white rounded-full overflow-hidden">
+        <div className="relative flex-grow flex-1 h-full bg-white rounded-full overflow-hidden min-h-[30px]">
             <div
-                className=" h-full transition-all duration-300"
+                className=" h-full transition-all duration-300 min-h-[30px]"
                 style={{ width: `${completed}%`, backgroundImage: 'var(--linear-main-gradient)' }}
             >
                 <span className="absolute inset-y-0 left-5 text-white font-bold inline-flex items-center h-full">
