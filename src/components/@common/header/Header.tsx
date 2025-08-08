0f498e53e2d687de43e33191c03cb053cd02df19
조하은 <jhe93@naver.com>
2025-08-08 15:44:51 +0900

    fix: 헤더 높이 지정

--- src/components/@common/header/Header.tsx
+++ src/components/@common/header/Header.tsx
@@ -6,7 +6,7 @@ import StartButton from './StartButton';
 
 function Header() {
     return (
-        <header className="fixed w-full z-100 mx-auto my-0 px-8 py-4 flex items-center justify-center box-border bg-white">
+        <header className="fixed w-full z-100 mx-auto my-0 px-8 py-4 flex items-center justify-center box-border bg-white h-[var(--header-height)]">
             <div className="flex flex-row flex-nowrap justify-between items-center w-full">
                 <div className="flex flex-row gap-2.5 items-center">
                     <LogoButton />
