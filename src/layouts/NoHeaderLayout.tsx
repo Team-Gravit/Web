e66cbbd1e738ec4bbe20ea628c5a9b8d00e36c01
조하은 <jhe93@naver.com>
2025-08-15 21:14:11 +0900

    feat(#39): 페이지 이동 시 스크롤 최상단 이동 구현

--- src/layouts/NoHeaderLayout.tsx
+++ src/layouts/NoHeaderLayout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '../components/@common/scroll/ScrollToTop';
 
 function NoHeaderLayout() {
     return (
         <div className="flex flex-col min-h-screen">
+            <ScrollToTop />
             <Outlet />
         </div>
     );
